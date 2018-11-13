1b92fc094693e63566ac43b68103ed5105334baa
FTC6567RoboRaiders <smkocik23@yahoo.com>
2018-11-13 06:11:09 -0500

    Updates to EncoderTest to specify the correct power values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -40,7 +40,7 @@ public class EncoderTest extends LinearOpMode {
             telemetry.addData("Status ", "opModeIsActive");
 
 
-            robot.setDriveMotorPower(0.5, 0.50, 0.50, 0.05);   // Set power to 50%
+            robot.setDriveMotorPower(0.5, 0.50, 0.50, 0.5);   // Set power to 50%
             telemetry.addData("Left Rear Drive Encoder Counts", "(%.0f)",robot.getBackLeftDriveEncoderCounts());
             telemetry.addData("Left Front Drive Encoder Counts", "(%.0f)",robot.getFrontLeftDriveEncoderCounts());
             telemetry.addData("Right Rear Drive Encoder Counts", "(%.0f)", robot.getBackRightDriveEncoderCounts());
