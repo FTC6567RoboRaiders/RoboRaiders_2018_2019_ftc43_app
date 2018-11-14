0b5fb9faa8c18f9a8be0440576fa8ebe35581baa
nickurbin <nicksurbin@frontier.com>
2018-11-14 15:35:45 -0500

    updated the redfar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboraiderAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboraiderAutonomous.java
@@ -28,8 +28,7 @@ public abstract class RoboraiderAutonomous extends LinearOpMode {
 
         EncoderDrivePID(robotPID, robot, 60);
         Thread.sleep(500);
-
-
+        
         }
 
      public void closeRedDepot (RoboRaidersPID robotPID, ProtoBot robot) throws InterruptedException {
