b78ac534e7e4e44bd001dc139b9e4e7b5aa18ef7
FTC6567RoboRaiders <smkocik23@yahoo.com>
2018-11-28 16:01:24 -0500

    Created AutoCrater - used for starting in front of crater

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrater.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import RoboRaiders.AutoOptions.RoboRaidersPID;
+
+@Autonomous(name="Auto: Robot Starts Towards Crater")
+
+public class AutoCrater extends RoboraiderAutonomous {
+
+    ProtoBot robot = new ProtoBot();
+    RoboRaidersPID robotPID = new RoboRaidersPID();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.initialize(hardwareMap);
+
+        waitForStart();
+
+        //DeployRobot(robot);
+
+        closeRedDepot(robotPID, robot);
+
+        //moveTest(robotPID, robot);
+
+    }
+
+    }
+
