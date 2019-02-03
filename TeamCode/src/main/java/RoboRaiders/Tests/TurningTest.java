4541bbae1055e5e67fbf94a8cbfe32a026074f54
anna <khakhalina.anna@gmail.com>
2019-02-03 14:23:34 -0500

    finished TurningTest

--- TeamCode/src/main/java/RoboRaiders/Tests/TurningTest.java
+++ TeamCode/src/main/java/RoboRaiders/Tests/TurningTest.java
@@ -11,6 +11,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+import RoboRaiders.Robot.NostromoBot;
+import RoboRaiders.Robot.NostromoBotMotorDumper;
+
 
 @TeleOp(name="TurningTest", group="Samples")
 
@@ -19,6 +22,11 @@ public class TurningTest extends LinearOpMode {
     public float degreesToTurn;
     public float currentHeading;
     public float finalHeading;
+    public double integratedZAxis;
+    public double iza_lastHeading = 0.0;
+    public double iza_deltaHeading;
+    public float iza_newHeading;
+    public Orientation iza_angles;
     Orientation angles;
     BNO055IMU imu;
 
@@ -35,21 +43,53 @@ public class TurningTest extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
+
         angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        degreesToTurn = 20;
+        degreesToTurn = 190;
         //telemetry.addLine().addData("degreesToTurn",String.valueOf(degreesToTurn));
         currentHeading = angles.firstAngle;
         finalHeading = currentHeading + degreesToTurn;
         //telemetry.update();
 
 
-
-        while(opModeIsActive() && currentHeading < finalHeading) {
+        while(opModeIsActive() && getIntegratedZAxis() < finalHeading) {
             angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             currentHeading = angles.firstAngle;
             telemetry.addLine().addData("getHeading",String.valueOf(currentHeading));
+            telemetry.addLine().addData("IntZ",String.valueOf(integratedZAxis));
             telemetry.update();
         }
 
     }
+
+    public double getIntegratedZAxis() {
+
+        // This sets up the how we want the IMU to report data
+        iza_angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        // Obtain the heading (Z-Axis)
+        iza_newHeading = iza_angles.firstAngle;
+
+        // Calculate the change in the heading from the previous heading
+        iza_deltaHeading = iza_newHeading - iza_lastHeading;
+
+        // Bosch IMU wraps at 180, so compensate for this
+        if (iza_deltaHeading <= -180.0) {
+
+            iza_deltaHeading += 360.0;
+        }
+        else if (iza_deltaHeading >= 180.0) {
+
+            iza_deltaHeading -= 360;
+        }
+
+        // Calculate the integratedZAxis
+        integratedZAxis += iza_deltaHeading;
+
+        // Save the current heading for the next call to this method
+        iza_lastHeading = iza_newHeading;
+
+        return integratedZAxis;
+    }
 }
+
