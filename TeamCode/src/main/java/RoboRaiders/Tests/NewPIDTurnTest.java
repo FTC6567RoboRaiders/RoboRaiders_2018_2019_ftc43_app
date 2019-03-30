d8203814c0e6beaa96e782a6fe7428561dd2f437
Roboraiders164 <roboraiders164@gmail.com>
2019-03-30 10:03:39 -0400

    new PID Turn Test

---
+++ TeamCode/src/main/java/RoboRaiders/Tests/NewPIDTurnTest.java
@@ -0,0 +1,71 @@
+package RoboRaiders.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import RoboRaiders.AutonomousMethods.AutoOptions.RoboRaidersPID;
+import RoboRaiders.AutonomousMethods.NostromoAutonomousMethods;
+import RoboRaiders.Robot.NostromoBotMotorDumper;
+import RoboRaiders.Robot.PidUdpReceiver;
+import RoboRaiders.Robot.RobotTelemetryDisplay;
+
+@Autonomous
+public class NewPIDTurnTest extends NostromoAutonomousMethods {
+    public NostromoBotMotorDumper robot = new NostromoBotMotorDumper();
+
+
+    private PidUdpReceiver pidUdpReceiver;
+    private RoboRaidersPID rrPID;
+    private RobotTelemetryDisplay rtd;
+    private double kP, kI, kD, direction, degrees = 45;
+
+
+    //----------------------------------------------------------------------------------------------
+    // Main logic
+    //----------------------------------------------------------------------------------------------
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Create new instance of robot telemetry display
+        //rtd = new RobotTelemetryDisplay(this,"Nostromo");
+
+
+        //Create new rrPID
+        rrPID = new RoboRaidersPID(0.02,0,0.1);
+
+
+
+
+        // initialize the robot
+        robot.initialize(hardwareMap);
+
+
+
+        // Wait for start to be pushed
+        waitForStart();
+
+        if (opModeIsActive()) {
+
+
+
+                imuTurnPID(rrPID, robot, (float)degrees, String.valueOf("right"));
+
+        }
+
+        pidUdpReceiver.shutdown();
+    }
+
+
+    public void updatePIDCoefficients() {
+
+        kP = pidUdpReceiver.getP();
+        kI = pidUdpReceiver.getI();
+        kD = pidUdpReceiver.getD();
+        direction = pidUdpReceiver.getDirection();
+        degrees = pidUdpReceiver.getDegrees();
+
+        //rtd.displayRobotTelemetry("kP",String.valueOf(kP));
+    }
+}
+
+
